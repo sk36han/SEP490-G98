bf54964278772228be53b61891d6810742dbafdb
khanhbucon <149470422+khanhbucon@users.noreply.github.com>
2026-01-12 22:52:17 +0700

    add base code

---
+++ WarehouseManagement.Domain/IGenericRepository/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement.Domain.IGenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T? GetById(int id);
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        void Delete(T entity);
+        IQueryable<T> GetQuery();
+        Task<IQueryable<T>> GetQuery(Expression<Func<T, bool>> predicate);
+        IQueryable<T> Get(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string includeProperties = "");
+    }
+
+}
---
+++ WarehouseService.Api/Helper/MapperClass.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace WarehouseService.Api.Helper
+{
+    public class MapperClass : Profile
+    {
+        public MapperClass()
+        {
+            // add automapper configurations here
+        }
+    }
+}
