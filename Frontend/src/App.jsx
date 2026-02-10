338692be4f341eb33b3a56125f19cf7677725809
Phúc Nguyên <ssclaodai@gmail.com>
2026-02-10 23:31:41 +0700

    Added Reset Password UI Page, Fix UI of Forgot Password

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword'
+import ResetPassword from './pages/ResetPassword/ResetPassword'
 import Profile from './pages/Profile/Profile'
 import './App.css'
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
